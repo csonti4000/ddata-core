bc8a1742f360cd48592619dc4615432dbe20dc7d
Bakos Bálint <bakosbalint4000@gmail.com>
2021-10-27 21:56:31 +0200

    fix list and view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DdataUiInputModule, InputHelperService } from 'ddata-ui-input';
 import { DdataCoreModule, ValidatorService } from 'ddata-core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
+import { DdataUiCommonModule } from 'ddata-ui-common';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
     RouterModule.forRoot([]),
     FormsModule,
     NgxDaterangepickerMd.forRoot(),
+    DdataUiCommonModule
   ],
   providers: [
     InputHelperService,
